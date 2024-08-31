07e6d184102e197051f64506b4f8f04470548858
shamliya <shamliyagcek@gmail.com>
2024-08-31 23:49:26 +0530

    first commit

--- src/imageModal/imageModal.js
+++ src/imageModal/imageModal.js
@@ -4,23 +4,22 @@ import { FaArrowLeft, FaArrowRight, FaDownload, FaTimes } from "react-icons/fa";
 import { saveAs } from "file-saver";
 
 function ImageModal({ imageUrl, onClose, onPrev, onNext }) {
-
-    const handleDownload = () => {
-        saveAs(imageUrl, "eventhex"); 
-      };
+  const handleDownload = () => {
+    saveAs(imageUrl, "eventhex");
+  };
 
   return (
     <div className="image-modal-overlay" onClick={onClose}>
       <div className="image-modal" onClick={(e) => e.stopPropagation()}>
         <button className="close-button" onClick={onClose}>
           <FaTimes size={24} />
-        </button>   
+        </button>
         <img src={imageUrl} alt="Full View" className="full-image" />
         <div className="navigation-buttons">
           <button className="nav-button" onClick={onPrev}>
             <FaArrowLeft size={20} />
           </button>
-          <button className="download-button"  onClick={handleDownload}>
+          <button className="download-button" onClick={handleDownload}>
             <FaDownload size={20} />
           </button>
           <button className="nav-button" onClick={onNext}>
